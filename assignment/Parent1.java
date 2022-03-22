cc74b67099ec2c466d7c9d0bff2f84a0d5705de8
unknown <balaharish480@gmail.com>
2022-03-22 09:51:19 -0700

    new

---
+++ assignment/Parent1.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Parent1 {
+	public Parent1()
+	{
+		System.out.println("class car");
+	}
+	public void vehicletype()
+	{
+		System.out.println("class BMW");
+	}
+
+}
+
